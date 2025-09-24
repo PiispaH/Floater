7826e307ff66993983bbaa8a7c009c87b89179d6
Henrik Koski <henrikkoski00@gmail.com>
2025-09-24 13:55:47 +0300

    Make linux friendly

---
+++ float.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    return f"JouJou mitä mites? {float("9.9") + 0.1}"
+
+
+if __name__ == "__main__":
+    print(main())
+
--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@
 #include <stdexcept>
 #include <array>
 
-#include "src\handle_strings.hpp"
+#include "src/handle_strings.hpp"
 
 
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
-    std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
     if (!pipe) {
-        throw std::runtime_error("_popen() failed!");
+        throw std::runtime_error("popen() failed!");
     }
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
         result += buffer.data();
@@ -32,7 +32,10 @@ int main(int argc, char** argv) {
     std::string new_file = convert_floaters(file);
     std::cout << "\nConversion completed, float on!" << std::endl;
     std::cout << "======= Program output =======\n\n";
-    std::string result = exec(new_file.c_str());
+    std::string result = exec(("python3 ./" + new_file).c_str());
     std::cout << result << std::endl;
     return 0;
 }
+
+
+
